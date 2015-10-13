cef08c12b92f024a069e27c4c2f3f944476f5d3f
MateuszMateo <k.mateusz120@gmail.com>
2015-10-13 19:08:17 +0200

    Szkic kodu
    "Indiana Jones Poszukiwacze błędów w kodzie"

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+long long int wartosciTrojkata[3000][3000];
+
+long long Symbol(long long N,long long K){
+    for(int N=0;N < 3000; ++N) {
+        wartosciTrojkata[N][0] = 1;
+        wartosciTrojkata[N][N] = 1;
+      for(int K=0; K <= N; ++K) {
+        wartosciTrojkata[N][K] = wartosciTrojkata[N-1][K-1] + wartosciTrojkata[N-1][K];
+      }
+    }
+
+
+}
+
+int main()
+{
+    cout << "Podaj wartosci" << endl;
+    long long N,K;
+    cin >> N >> K;
+    cout << Symbol(N,K)<<endl;
+    return 0;
+}
+
