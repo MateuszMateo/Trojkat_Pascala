8c55092f4b950b65cb4e92be8d4bd71092819870
MateuszMateo <k.mateusz120@gmail.com>
2015-10-28 21:05:17 +0100

    poprawa funkcji wpisującej wartości

--- main.cpp
+++ main.cpp
@@ -4,22 +4,23 @@ using namespace std;
 
 long long int wartosciTrojkata[3000][3000];
 
-long long Symbol(long long N,long long K){
-    for(int N=0;N < 3000; ++N) {
+long long Symbol(){
+    for(int N=0;N < 2999; ++N) {
         wartosciTrojkata[N][0] = 1;
         wartosciTrojkata[N][N] = 1;
-      for(int K=0; K <= N; ++K) {
-        wartosciTrojkata[N][K] = wartosciTrojkata[N-1][K-1] + wartosciTrojkata[N-1][K];
+      for(int K=1; K <= N; ++K) {
+        wartosciTrojkata[N][K] = wartosciTrojkata[N-1][K-1] + wartosciTrojkata[N][K-1];
 
-      }
+
     }
 
 
 }
-
+}
 int main()
 {
     cout << "Podaj wartosci" << endl;
+    Symbol();
     long long n,k;
     cin >> n >> k;
     cout << wartosciTrojkata[n][k]<<endl;
